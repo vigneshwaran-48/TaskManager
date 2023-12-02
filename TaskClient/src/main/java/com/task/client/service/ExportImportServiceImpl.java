1ceecb2662140a3c460af488bba28a57e76cd894
vigneshwaran48 <p3487260@gmail.com>
2023-12-02 22:31:46 +0530

    Added service class in client for export import

---
+++ TaskClient/src/main/java/com/task/client/service/ExportImportServiceImpl.java
@@ -0,0 +1,58 @@
+package com.task.client.service;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import com.task.library.dto.NoDataResponse;
+import com.task.library.exception.AppException;
+import com.task.library.service.ExportImportService;
+
+@Service
+public class ExportImportServiceImpl implements ExportImportService {
+
+    @Autowired
+    private WebClient webClient;
+
+    @Value("${app.resource.server.baseurl}")
+    private String resourceServerBaseURL;
+
+    private final static String BASE_PATH = "/api/v1/data";
+    private final static String SLASH = "/";
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ExportImportServiceImpl.class);
+
+    @Override
+    public Optional<byte[]> exportData(String userId) throws AppException {
+        StringBuffer urlBuffer = new StringBuffer(resourceServerBaseURL);
+        urlBuffer.append(SLASH).append(BASE_PATH).append(SLASH).append("export");
+
+        byte[] data = webClient.get()
+                            .uri(urlBuffer.toString())
+                            .retrieve()
+                            .bodyToMono(byte[].class)
+                            .block();
+        return Optional.of(data);
+    }
+
+    @Override
+    public void importData(String userId, byte[] data) throws AppException {
+        StringBuffer urlBuffer = new StringBuffer(resourceServerBaseURL);
+        urlBuffer.append(SLASH).append(BASE_PATH).append(SLASH).append("import");
+
+        NoDataResponse response = webClient
+                                        .post()
+                                        .uri(urlBuffer.toString())
+                                        .retrieve()
+                                        .bodyToMono(NoDataResponse.class)
+                                        .block();
+
+        LOGGER.info("Imported data response from resource server => {}", response);
+    }
+    
+}
