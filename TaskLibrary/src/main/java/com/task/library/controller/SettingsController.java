d19d3c7bbb49a741e84af267381be311e7dc4740
vigneshwaran48 <p3487260@gmail.com>
2023-12-10 13:37:14 +0530

    Added patch endpoint settings controller

--- TaskLibrary/src/main/java/com/task/library/controller/SettingsController.java
+++ TaskLibrary/src/main/java/com/task/library/controller/SettingsController.java
@@ -11,10 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.task.library.dto.setting.SettingsDataResponse;
+import com.task.library.dto.setting.Sort;
+import com.task.library.dto.setting.SortGroupBy;
+import com.task.library.dto.setting.Theme;
 import com.task.library.dto.NoDataResponse;
 import com.task.library.dto.setting.SettingsDTO;
 import com.task.library.exception.AppException;
@@ -36,6 +40,12 @@ public class SettingsController {
     private final static String BASE_PATH = "/api/v1/settings";
 	private final static String NOT_AUTHENTICATED = "Not Authenticated";
     private Logger LOGGER = LoggerFactory.getLogger(SettingsController.class);
+
+    private static final String SHOULD_GROUP_TASKS = "shouldGroupTasks";
+    private static final String SORT_BY = "sortBy";
+    private static final String SORT_GROUP_BY = "sortGroupBy";
+    private static final String THEME = "theme";
+    
 
     @GetMapping
     public ResponseEntity<?> getSettings(Principal principal) throws AppException {
@@ -105,7 +115,46 @@ public class SettingsController {
         response.setStatus(HttpStatus.OK.value());
         response.setTime(LocalDateTime.now());
 
-        return new ResponseEntity<SettingsDataResponse>(response, HttpStatus.OK);
+        return ResponseEntity.ok(response);
     }
     
+    @PatchMapping
+    public ResponseEntity<?> updateSettings(
+                        @RequestBody String settingOption, 
+                        @RequestBody String value, Principal principal) throws AppException {
+
+        StringBuffer userId = new StringBuffer(principal != null ? principal.getName() : "");
+		if(!AuthUtil.getInstance().isValidUserId(userId)) {
+			throw new AppException(NOT_AUTHENTICATED, HttpStatus.BAD_REQUEST.value());
+		}
+        
+        SettingsDTO settingsDTO = settingsService.getSettings(userId.toString()).orElse(null);
+
+        switch (settingOption) {
+            case SHOULD_GROUP_TASKS:
+                settingsDTO.setShouldGroupTasks(Boolean.parseBoolean(value));
+                break;
+            case SORT_BY:
+                settingsDTO.setSortBy(Sort.valueOf(value));
+                break;
+            case SORT_GROUP_BY:
+                settingsDTO.setSortGroupBy(SortGroupBy.valueOf(value));
+                break;
+            case THEME:
+                settingsDTO.setTheme(Theme.valueOf(value));
+                break;
+            default:
+                throw new AppException("Invalid settings option", HttpStatus.BAD_REQUEST.value());
+        }
+        settingsDTO = settingsService.updateSettings(userId.toString(), settingsDTO).orElse(null);
+
+        SettingsDataResponse response = new SettingsDataResponse();
+        response.setData(settingsDTO);
+        response.setMessage("success");
+        response.setPath(BASE_PATH);
+        response.setStatus(HttpStatus.OK.value());
+        response.setTime(LocalDateTime.now());
+
+        return ResponseEntity.ok(response);
+    }
 }
